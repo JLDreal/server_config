cc723811094ad5d5790903a2bc23c521933a88ac
Y <you@example.com>
2025-10-30 16:30:41 +0100

    1

---
+++ configuration.nix
@@ -0,0 +1,110 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+      git
+      gum
+      (
+        writeShellScriptBin "nix_installer"
+        ''
+          #!/usr/bin/env bash
+          set -euo pipefail
+
+
+          if [ "$(id -u)" -eq 0 ]; then
+          	echo "ERROR! $(basename "$0") should be run as a regular user"
+          	exit 1
+          fi
+
+          if [ ! -d "$HOME/dotfiles/.git" ]; then
+          	git clone https://gitlab.com/hmajid2301/dotfiles.git "$HOME/dotfiles"
+          fi
+
+          TARGET_HOST=$(ls -1 ~/dotfiles/hosts/*/configuration.nix | cut -d'/' -f6 | grep -v iso | gum choose)
+
+          if [ ! -e "$HOME/dotfiles/hosts/$TARGET_HOST/disks.nix" ]; then
+          	echo "ERROR! $(basename "$0") could not find the required $HOME/dotfiles/hosts/$TARGET_HOST/disks.nix"
+          	exit 1
+          fi
+
+          gum confirm  --default=false \
+          "🔥 🔥 🔥 WARNING!!!! This will ERASE ALL DATA on the disk $TARGET_HOST. Are you sure you want to continue?"
+
+          echo "Partitioning Disks"
+          sudo nix run github:nix-community/disko \
+          --extra-experimental-features "nix-command flakes" \
+          --no-write-lock-file \
+          -- \
+          --mode zap_create_mount \
+          "$HOME/dotfiles/hosts/$TARGET_HOST/disks.nix"
+
+          sudo nixos-install --flake "$HOME/dotfiles#$TARGET_HOST"
+        ''
+      )
+    ];
+
+  services.openssh = {
+      enable = true;
+      ports = [ 22 ];
+      settings = {
+        PasswordAuthentication = true;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = [ "hans" ];
+      };
+    };
+    networking = {
+        usePredictableInterfaceNames = false;
+        interfaces.eth0.ipv4.addresses = [{
+          address = "10.1.104.202";
+          prefixLength = 16;
+        }];
+        defaultGateway = "10.1.104.90";
+        nameservers = [ "10.1.104.1" ];
+      };
+      nixpkgs = {
+        hostPlatform = lib.mkDefault "x86_64-linux";
+        config.allowUnfree = true;
+      };
+
+      nix = {
+        settings.experimental-features = ["nix-command" "flakes"];
+        extraOptions = "experimental-features = nix-command flakes";
+      };
+
+
+
+      boot = {
+        kernelPackages = pkgs.linuxPackages_latest;
+        supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
+      };
+
+      networking = {
+        hostName = "sklave1";
+      };
+
+      # gnome power settings do not turn off screen
+      systemd = {
+        services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
+        targets = {
+          sleep.enable = false;
+          suspend.enable = false;
+          hibernate.enable = false;
+          hybrid-sleep.enable = false;
+        };
+      };
+
+      users.users.hans = {
+        isNormalUser = true;
+        password = "egon34";
+        description = "hans";
+        extraGroups = [ "networkmanager" "wheel" "sudo" "root" ];
+        packages = with pkgs; [
+          git
+          curl
+        ];
+      };
+      users.extraUsers.root.password = "egon34";
+}
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "My Home Manager configuration";
+
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    zed.url = "github:zed-industries/zed";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:JLDreal/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+
+     lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+  };
+outputs = {self, nixpkgs, home-manager, zen-browser, nixpkgs-unstable,lix, lix-module,zed,flake-parts , ... }@inputs:
+let
+
+  system = "x86_64-linux";
+  lib = nixpkgs.lib;
+
+
+  pkgs = import nixpkgs {
+    inherit system;
+    config = {
+      allowUnfree = true;
+      allowBroken = true;
+    };
+  };
+in {
+
+  environment.systemPackages = with pkgs; [ git
+
+  ];
+
+  nixosConfigurations = {
+    iso = lib.nixosSystem {
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+        ./configuration.nix
+      ];
+      specialArgs = {inherit inputs;};
+    };
+  };
+
+};}
